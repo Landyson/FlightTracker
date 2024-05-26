910931acf8af82433e8d265f9a2dd46ef1b20af9
Landyson <landamartin220@gmail.com>
2024-05-26 21:08:58 +0200

    Prepared methods for UNIT test

---
+++ src/test/java/org/example/PlaneDataTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.opensky.model.StateVector;
+
+
+
+class PlaneDataTest {
+
+    private StateVector mockStateVector;
+
+    @org.junit.jupiter.api.Test
+    void load() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void randomGeneratePlanes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void generateCallsign() {
+    }
+}
